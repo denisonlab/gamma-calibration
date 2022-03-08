27aa6787225a943813984fd05a896389fada4fb1
karentian <9136901+karentian@users.noreply.github.com>
2022-03-08 15:24:52 -0500

    plot luminance

---
+++ plotGamma.m
@@ -0,0 +1,35 @@
+function plotGamma
+
+%% load gamma data 
+dataName = 'gammaCalibration_DenLab-EEG_20220304-1444.mat'; 
+% 'gammaCalibration_DenLab-EEG_20220304-1444.mat'
+% 'gammaCalibration_DenLab-Behav_20220304-1247.mat'
+dataPath = sprintf('%s/data/%s', pwd, dataName); 
+data = load(dataPath); % data.colorVals, data.luminance  
+
+%% Plot 
+figure
+scatter(data.colorVals, data.luminance) 
+figTitle = dataName(1:end-4); 
+title(sprintf('%s', und2space(figTitle))) 
+xlabel('Color val (step size = 5)')
+ylabel('Luminance') 
+
+% format 
+box off
+grid on
+axis square 
+xlim([0 255])
+set(gca,'TickDir','out');
+ax = gca;
+ax.LineWidth = 1.5;
+ax.XColor = 'black';
+ax.YColor = 'black';
+ax.FontSize = 14;
+
+%% Save figure 
+figDir = sprintf('%s/figures', pwd); 
+if ~exist(figDir, 'dir')
+    mkdir(figDir)
+end
+saveas(gcf,sprintf('%s/%s.png', figDir, figTitle))
